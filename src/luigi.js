5a02ff53e852ac95bd86b0528a85066c8016e010
Daneliz Urena <durena402@gmail.com>
2019-11-23 14:53:57 -0500

    almost done

--- src/luigi.js
+++ src/luigi.js
@@ -8,18 +8,21 @@ let num = Number(readlineSync.question("\nHeight: "));
 while ((num < Min) || (num > Max) || Number.isNaN(num) || (!Number.isInteger(num)))  {
   num = Number(readlineSync.question("Height: "));
 }
-
+console.log("");
 let row;
-
+let value = "#";
 for (let i = 1; i <= num; i++) {
   row = "";
-
+  second = "";
   for (let x = 1; x <= (num - i); x++) {
     row = row + " ";
   }
-
   for (let y = 1; y <= i; y++) {
     row = row + "#";
   }
-  console.log(row + "#");
+  for (let y = 1; y <= i; y++) {
+    second = second + "#";
+  }
+  console.log(row + "#" + "  "+ second + "#");
 }
+console.log("");
